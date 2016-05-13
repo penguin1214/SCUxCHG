61a4860dc61f8bce01bef070e6504997f4338820
penguin-penpen <penguingohack@outlook.com>
2016-05-13 15:55:22 +0800

    add JSONModel entity, send to backend success

---
+++ SCUxCHG/BaseEntity.h
@@ -0,0 +1,21 @@
+//
+//  BaseEntity.h
+//  SCUxCHG
+//
+//  Created by 杨京蕾 on 5/10/16.
+//  Copyright © 2016 yang. All rights reserved.
+//
+
+#import <JSONModel/JSONModel.h>
+
+@interface BaseEntity : JSONModel
+
+/**
+ * 根实体，所有实体都必须继承它
+ *  自定义的对象必须用strong属性，如果用copy属性将会导致错误（WhichEntity copyWithZone:]: unrecognized selector sent to instance）
+ */
+
+
+@property (nonatomic, copy) NSString *id;
+
+@end
