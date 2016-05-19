03c7766c89f33a9877c1b4d81150af07cfe2a7e3
penguin-penpen <penguingohack@outlook.com>
2016-05-19 16:21:38 +0800

    add sellTabController

---
+++ SCUxCHG/SellTabController.h
@@ -0,0 +1,13 @@
+//
+//  SellTabController.h
+//  SCUxCHG
+//
+//  Created by 杨京蕾 on 5/19/16.
+//  Copyright © 2016 yang. All rights reserved.
+//
+
+#import "BaseController.h"
+
+@interface SellTabController : BaseController
+
+@end
---
+++ SCUxCHG/SellTabController.m
@@ -0,0 +1,13 @@
+//
+//  SellTabController.m
+//  SCUxCHG
+//
+//  Created by 杨京蕾 on 5/19/16.
+//  Copyright © 2016 yang. All rights reserved.
+//
+
+#import "SellTabController.h"
+
+@implementation SellTabController
+
+@end
